9e9b2a7aa05769fdcc3d31c1f412a946c20cb05d
Ivan Rodriguez E <ivanrenescorcia@gmail.com>
2021-09-26 23:04:06 -0500

    infrastructure added

--- src/core/database/database.config.ts
+++ src/core/database/database.config.ts
@@ -5,25 +5,25 @@ dotenv.config();
 
 export const databaseConfig: IDatabaseConfig = {
   development: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_NAME,
+    database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
   },
   test: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_NAME,
+    database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     dialect: 'postgres',
   },
   production: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_NAME,
+    database: process.env.POSTGRES_DB,
     port: process.env.POSTGRES_PORT,
     host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
